474ad99e4152aa4189059fc74f14bf79e659786b
VitorTavaresO <vitortavares.o55@gmail.com>
2023-10-30 12:16:47 -0300

    Start Moc

---
+++ moc/build/sketch/moc.ino.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#line 1 "C:\\Codes\\motor-control\\moc\\moc.ino"
+#line 1 "C:\\Codes\\motor-control\\moc\\moc.ino"
+void setup();
+#line 7 "C:\\Codes\\motor-control\\moc\\moc.ino"
+void loop();
+#line 1 "C:\\Codes\\motor-control\\moc\\moc.ino"
+void setup()
+{
+	Serial.begin(9600);
+    Serial.println("Hello World");
+}
+
+void loop()
+{
+	
+}
+
