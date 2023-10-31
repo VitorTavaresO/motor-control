01719b0d604c9b844dd7f741f8a215cd6807e5e1
Vitor Tavares <vitortavares.o55@gmail.com>
2023-10-31 13:55:33 -0300

    Update moc.ino

--- moc/moc.ino
+++ moc/moc.ino
@@ -1,10 +1,13 @@
+#define PIN A0
+
 void setup()
 {
-	Serial.begin(9600);
-    Serial.println("Hello World");
+	Serial.begin(115200);
+    pinMode(INPUT, INPUT);
 }
 
 void loop()
 {
-	
+    Serial.println(analogRead(PIN));
+    delay(1000);
 }
