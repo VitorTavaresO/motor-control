b859a8edd5f7d0886b6764cc6b5961a5dd8fc1be
Vitor Tavares <vitortavares.o55@gmail.com>
2024-01-03 09:27:55 -0300

    Correct - Temperature_Sensor_Pin_Const

--- motor-temperature.cpp
+++ motor-temperature.cpp
@@ -1,11 +1,12 @@
 #include <stdint.h>
 #include <Arduino.h>
+#include "const.h"
 
-uint16_t max_temperature = 102;
+uint16_t max_temperature = 1023;
 
 
 uint16_t temperature_read(){
-    uint16_t temperature_read = analogRead(A0);
+    uint16_t temperature_read = analogRead(TEMPERATURE_SENSOR_PIN);
 
     temperature_read = map(temperature_read, max_temperature, 0, 0, 100);
 
