78d5413d8cc32c21a9f914a945d96dd56c935056
Vitor Tavares <vitortavares.o55@gmail.com>
2024-08-09 17:16:05 -0300

    Testing Send Data to Rasp

--- src/main.ino
+++ src/main.ino
@@ -12,6 +12,12 @@ void setup()
 }
 void loop()
 {
-    Serial.println("Dados para o Raspberry Pi");
+    uint8_t data[5] = {analogRead(TEMPERATURE_PIN), analogRead(FUEL_PIN), analogRead(OIL_TEMPERATURE_PIN), analogRead(MOTOR_RPM), analogRead(BATTERY_VOLTAGE)};
+
+    Serial.println("Temperature: " + data[0]);
+    Serial.println("Fuel: " + data[1]);
+    Serial.println("Oil Temperature: " + data[2]);
+    Serial.println("Motor RPM: " + data[3]);
+    Serial.println("Battery Voltage: " + data[4]);
     delay(1000);
 }
