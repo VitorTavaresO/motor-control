52c9f48e9454aec8c3be32329a0ed19c2f3a731b
Vitor Tavares <vitortavares.o55@gmail.com>
2024-08-09 17:11:09 -0300

    Created Const.h file

---
+++ src/const.h
@@ -0,0 +1,9 @@
+
+#define SerialDebug 9600
+#define SerialRasp 115200
+
+#define TEMPERATURE_PIN A0
+#define FUEL_PIN A1
+#define OIL_TEMPERATURE_PIN A2
+#define MOTOR_RPM A3
+#define BATTERY_VOLTAGE A4
