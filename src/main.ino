3657dd3c689269bdce52b7a402e1a9067c981da8
VitorTavaresO <vitortavares.o55@gmail.com>
2024-10-16 14:54:09 -0300

    Motor Control Changes

--- src/main.ino
+++ src/main.ino
@@ -14,10 +14,6 @@ void loop()
 {
     uint8_t data[5] = {analogRead(TEMPERATURE_PIN), analogRead(FUEL_PIN), analogRead(OIL_TEMPERATURE_PIN), analogRead(MOTOR_RPM), analogRead(BATTERY_VOLTAGE)};
 
-    Serial.println("Temperature: " + data[0]);
-    Serial.println("Fuel: " + data[1]);
-    Serial.println("Oil Temperature: " + data[2]);
-    Serial.println("Motor RPM: " + data[3]);
-    Serial.println("Battery Voltage: " + data[4]);
+    Serial.write(data, sizeof(data));
     delay(1000);
 }
