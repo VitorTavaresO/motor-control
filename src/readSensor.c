16ece822fc51151ad44f627ba1110b48a779f9aa
Vitor Tavares <vitortavares.o55@gmail.com>
2024-11-01 14:17:39 -0300

    Feat - Created ReadSensor.c

---
+++ src/readSensor.c
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <const.h>
+
+uint8_t read_motor_temperature()
+{
+    return map(analogRead(TEMPERATURE_PIN), 0, 1023, 0, 255);
+}
+
+uint8_t read_fuel()
+{
+    return map(analogRead(FUEL_PIN), 0, 1023, 0, 255);
+}
+
+uint8_t read_oil_temperature()
+{
+    return map(analogRead(OIL_TEMPERATURE_PIN), 0, 1023, 0, 58);
+}
+
+uint8_t read_motor_rpm()
+{
+    return map(analogRead(MOTOR_RPM), 0, 1023, 0, 5800);
+}
+
+uint8_t read_battery_voltage()
+{
+    return map(analogRead(BATTERY_VOLTAGE), 0, 1023, 0, 100);
+}
